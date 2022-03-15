b76877c1c7790cf496110f891bd67ec3d49fe9e4
Gluszczuk <brunogluszczukjobs@gmail.com>
2022-03-14 23:20:40 -0300

    refactoring tests

--- src/auction/test_valuer.py
+++ src/auction/test_valuer.py
@@ -5,20 +5,21 @@ from src.auction.domain import User, Bid, Auction, Valuer
 
 class TestValuer(TestCase):
 
+    def setUp(self):
+        self.bruno = User('Bruno')
+        self.mari = User('Mari')
+        self.leia = User('Leia')
+        self.bid_from_bruno = Bid(self.bruno, 100)
+        self.bid_from_mari = Bid(self.mari, 150)
+        self.bid_from_leia = Bid(self.leia, 200)
+        self.auction = Auction('Laptop')
+
     def test_should_return_the_largest_and_smallest_when_added_in_ascending_order(self):
-        bruno = User('Bruno')
-        mari = User('Mari')
-
-        bid_from_bruno = Bid(bruno, 100)
-        bid_from_mari = Bid(mari, 150)
-
-        auction = Auction('Laptop')
-
-        auction.bids.append(bid_from_bruno)
-        auction.bids.append(bid_from_mari)
+        self.auction.bids.append(self.bid_from_bruno)
+        self.auction.bids.append(self.bid_from_mari)
 
         valuer = Valuer()
-        valuer.evaluate(auction)
+        valuer.evaluate(self.auction)
 
         lowest_bid_expected = 100.0
         highest_bid_expected = 150.0
@@ -27,19 +28,11 @@ class TestValuer(TestCase):
         self.assertEqual(highest_bid_expected, valuer.highest_bid_expected)
 
     def test_should_return_the_largest_and_smallest_when_added_in_descending_order(self):
-        bruno = User('Bruno')
-        mari = User('Mari')
-
-        bid_from_bruno = Bid(bruno, 100)
-        bid_from_mari = Bid(mari, 150)
-
-        auction = Auction('Laptop')
-
-        auction.bids.append(bid_from_mari)
-        auction.bids.append(bid_from_bruno)
+        self.auction.bids.append(self.bid_from_mari)
+        self.auction.bids.append(self.bid_from_bruno)
 
         valuer = Valuer()
-        valuer.evaluate(auction)
+        valuer.evaluate(self.auction)
 
         lowest_bid_expected = 100.0
         highest_bid_expected = 150.0
@@ -48,9 +41,7 @@ class TestValuer(TestCase):
         self.assertEqual(highest_bid_expected, valuer.highest_bid_expected)
 
     def test_should_return_the_same_value_for_the_highest_and_lowest_bid_when_the_auction_has_a_bid(self):
-        bruno = User('Bruno')
-
-        bid = Bid(bruno, 100)
+        bid = Bid(self.bruno, 100)
 
         auction = Auction('Book')
         auction.bids.append(bid)
@@ -62,22 +53,12 @@ class TestValuer(TestCase):
         self.assertEqual(100, valuer.lowest_bid)
 
     def test_should_return_the_highest_and_lowest_value_when_the_auction_has_three_bids(self):
-        bruno = User('Bruno')
-        mari = User('Mari')
-        leia = User('Leia')
-
-        bid_from_bruno = Bid(bruno, 100)
-        bid_from_mari = Bid(mari, 140)
-        bid_from_leia = Bid(leia, 200)
-
-        auction = Auction('Notebook')
-
-        auction.bids.append(bid_from_mari)
-        auction.bids.append(bid_from_bruno)
-        auction.bids.append(bid_from_leia)
+        self.auction.bids.append(self.bid_from_mari)
+        self.auction.bids.append(self.bid_from_bruno)
+        self.auction.bids.append(self.bid_from_leia)
 
         valuer = Valuer()
-        valuer.evaluate(auction)
+        valuer.evaluate(self.auction)
 
         lowest_bid_expected = 100.0
         highest_bid_expected = 200.0
